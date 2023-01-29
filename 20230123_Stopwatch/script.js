36079c19d0d50aa0d72d134bdaf3e560a254a717
kentshenlim <kentshen_lim24@hotmail.com>
2023-01-29 20:15:35 +0000

    Add counting function

--- 20230123_Stopwatch/script.js
+++ 20230123_Stopwatch/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   // Import constants and initialize global var
-  const msec = 0;
-  const sec = 0;
+  let msec = 0;
+  let sec = 0;
   const msecDisp = document.querySelector('span#milliseconds');
   const secDisp = document.querySelector('span#seconds');
   const startBtn = document.querySelector('button#start');
@@ -11,8 +11,18 @@ window.onload = function () {
 
   startBtn.addEventListener('click', () => {
     clearInterval(Interval);
-    setInterval(startCounting, 10); // Implement startCounting every 0.1 ms
+    setInterval(startCounting, 10); // Implement startCounting every 10 ms
   });
 
-  function startCounting() {}
+  function startCounting() {
+    msec += 1; // 10 ms = 0.01 s so +1 here (the msec not really in msec)
+    if (msec <= 9) msecDisp.textContent = `0${msec}`;
+    else if (msec <= 99) msecDisp.textContent = String(msec);
+    else {
+      msec = 0;
+      msec.textContent = '00';
+      sec += 1;
+    }
+    secDisp.textContent = sec <= 9 ? `0${sec}` : String(sec);
+  }
 };
