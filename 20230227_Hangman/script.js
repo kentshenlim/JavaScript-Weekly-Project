5c4517d47881c0ec1a0d3ba20be919204452ddf0
kentshenlim <kentshen_lim24@hotmail.com>
2023-03-05 14:47:24 +0000

    Add element names

---
+++ 20230227_Hangman/script.js
@@ -0,0 +1,54 @@
+(function () {
+  const display = {
+
+  };
+  const dictionary = {
+    'Alkali Metal': {
+      lithium: '',
+      sodium: '',
+      potassium: '',
+      rubidium: '',
+      caesium: '',
+      francium: '',
+    },
+    'Alkaline Earth Metal': {
+      beryllium: '',
+      magnesium: '',
+      calcium: '',
+      strontium: '',
+      barium: '',
+      radium: '',
+    },
+    Pnictogen: {
+      nitrogen: '',
+      phosphorus: '',
+      arsenic: '',
+      antimony: '',
+      bismuth: '',
+    },
+    Chalcogen: {
+      oxygen: '',
+      sulfur: '',
+      selenium: '',
+      tellurium: '',
+      polonium: '',
+    },
+    Halogen: {
+      fluorine: '',
+      chlorine: '',
+      bromine: '',
+      iodine: '',
+      astatine: '',
+
+    },
+    'Noble gas': {
+      helium: '',
+      neon: '',
+      argon: '',
+      krypton: '',
+      xenon: '',
+      radon: '',
+    },
+
+  };
+}());
